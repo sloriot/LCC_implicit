88788f54f289a04a5f98d33711111daee2963dfe
Sébastien Loriot <sebastien.loriot@cgal.org>
2023-12-22 00:40:50 +0100

    use a triangulation with indexed vertices

--- test.cpp
+++ test.cpp
@@ -11,12 +11,44 @@ using LCC = CGAL::Linear_cell_complex_for_combinatorial_map<3,3, CGAL::Linear_ce
 using Dart_descriptor =  LCC::Dart_descriptor;
 using Point = K::Point_3;
 
-//~ typedef CGAL::Linear_cell_complex_for_combinatorial_map<3> LCC_3;
-//~ typedef LCC_3::Dart_descriptor Dart_descriptor;
-//~ typedef LCC_3::Point           Point;
+template < typename GT,
+           typename Vb = CGAL::Triangulation_vertex_base_3<GT> >
+class Triangulation_vertex_base_with_id_3
+  : public Vb
+{
+  std::size_t _id;
+public:
 
-using DT = CGAL::Delaunay_triangulation_3<K>;
+  typedef typename Vb::Cell_handle                   Cell_handle;
+  typedef typename Vb::Point                         Point;
 
+  template < typename TDS2 >
+  struct Rebind_TDS {
+    typedef typename Vb::template Rebind_TDS<TDS2>::Other          Vb2;
+    typedef Triangulation_vertex_base_with_id_3<GT, Vb2>   Other;
+  };
+
+  Triangulation_vertex_base_with_id_3()
+    : Vb() {}
+
+  Triangulation_vertex_base_with_id_3(const Point & p)
+    : Vb(p) {}
+
+  Triangulation_vertex_base_with_id_3(const Point & p, Cell_handle c)
+    : Vb(p, c) {}
+
+  Triangulation_vertex_base_with_id_3(Cell_handle c)
+    : Vb(c) {}
+
+  const std::size_t& id() const { return _id; }
+  std::size_t&       id()       { return _id; }
+};
+
+
+using Vb = Triangulation_vertex_base_with_id_3<K>;
+using Cb = CGAL::Delaunay_triangulation_cell_base_3<K>;
+using TDS = CGAL::Triangulation_data_structure_3<Vb, Cb>;
+using DT = CGAL::Delaunay_triangulation_3<K, TDS>;
 
 int main()
 {
@@ -30,6 +62,14 @@ int main()
 
 
   DT dt(v.begin(), v.end());
+  std::size_t id=0;
+  std::vector<DT::Vertex_handle> vertices(nb_points);
+  for (DT::Vertex_handle vh : dt.finite_vertex_handles())
+  {
+    vh->id()=id++;
+    vertices[vh->id()]=vh;
+  }
+
 
   LCC lcc;
   std::map<DT::Cell_handle, Dart_descriptor > vol_to_dart;
